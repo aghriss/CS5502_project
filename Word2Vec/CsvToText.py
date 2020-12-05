1d6c4a1de884a89c110da642fb26a597d7b94b18
Nimit Patel <nipa1388@colorado.edu>
2020-12-05 12:53:37 -0500

    Refactored some code; started to write code to get text from CSV columns

---
+++ Word2Vec/CsvToText.py
@@ -0,0 +1,9 @@
+
+
+
+
+
+import pandas as pd
+
+rf = pd.read_csv("Arizona.csv", error_bad_lines=False)
+print(rf)
--- Word2Vec/FeatureExtractor.py
+++ Word2Vec/FeatureExtractor.py
@@ -5,72 +5,78 @@ import nltk
 warnings.filterwarnings(action='ignore')
 
 
-# Get a list of bigrams sorted by their frequency
-# [("I", "am"), ("to", "the"), ...]
-def getBigrams(s):
-    tokens = nltk.word_tokenize(s)
-    bgs = nltk.bigrams(tokens)
-    fdist = nltk.FreqDist(bgs)
+class FeatureExtractor:
+    def __init__(self, file1, file2):
+        biExt = BigramExtractor()
+        bigrams1 = biExt.getBigramsFromFile(file1)
+        bigrams2 = biExt.getBigramsFromFile(file2)
+        compareTop = 700
+        numFeats = 100
 
-    d = dict(fdist)
-    lst = list(d.items())
-    lst.sort(key=lambda x:x[1], reverse=True)
+        feat = self.getUniqFeat(bigrams1, bigrams2, compareTop, numFeats / 2)
+        feat += self.getUniqFeat(bigrams2, bigrams1, compareTop, numFeats / 2)
 
-    return list(dict(lst).keys())
-
-# Wrapper for the previous function
-# Read the file and return bigrams
-def getBigramsFromFile(fileName):
-    sample = open(fileName, "r", encoding="utf8")
-    s = sample.read()
-    return getBigrams(s)
-
-
-# It goes through top (compareTop) elements in the bigrams1 list
-# If the item is not found in the top (compareTop) of bigrams2, it's added to the list
-# This would mean that the item is common in bigrams1, but not in bigrams2 so it can be a good feature
-def getUniqFeat(bigrams1, bigrams2, compareTop, limit):
-    features = []
-    for indX in range(compareTop):
-        x = bigrams1[indX]
-        indY = len(bigrams2)
-        try:
-            indY = bigrams2.index(x)
-        except:
-            pass
-
-        if indY > compareTop:
-            features.append(x)
-
-        if len(features) == limit:
-            return features
-    return features
+        self.features = set(feat)
 
 
 
-bigrams1 = getBigramsFromFile("alice.txt")
-bigrams2 = getBigramsFromFile("queen.txt")
-compareTop = 700
-numFeats = 100
+    # It goes through top (compareTop) elements in the bigrams1 list
+    # If the item is not found in the top (compareTop) of bigrams2, it's added to the list
+    # This would mean that the item is common in bigrams1, but not in bigrams2 so it can be a good feature
+    def getUniqFeat(self, bigrams1, bigrams2, compareTop, limit):
+        features = []
+        for indX in range(compareTop):
+            x = bigrams1[indX]
+            indY = len(bigrams2)
+            try:
+                indY = bigrams2.index(x)
+            except:
+                pass
 
-feat = getUniqFeat(bigrams1, bigrams2, compareTop, numFeats / 2)
-feat += getUniqFeat(bigrams2, bigrams1, compareTop, numFeats / 2)
+            if indY > compareTop:
+                features.append(x)
 
-# for i, f in enumerate(feat):
-#     print(f'{i}: {f}')
-
-feat = set(feat)
-
+            if len(features) == limit:
+                return features
+        return features
 
 
-# CSV to text
-# row + features = features on
+class BigramExtractor:
+    # Get a list of bigrams sorted by their frequency
+    # [("I", "am"), ("to", "the"), ...]
+    def getBigrams(self, s):
+        tokens = nltk.word_tokenize(s)
+        bgs = nltk.bigrams(tokens)
+        fdist = nltk.FreqDist(bgs)
 
+        d = dict(fdist)
+        lst = list(d.items())
+        lst.sort(key=lambda x: x[1], reverse=True)
+
+        return list(dict(lst).keys())
+
+    # Wrapper for the previous function
+    # Read the file and return bigrams
+    def getBigramsFromFile(self, fileName):
+        sample = open(fileName, "r", encoding="utf8")
+        s = sample.read()
+        return self.getBigrams(s)
+
+
+
+featureExt = FeatureExtractor("alice.txt", "queen.txt")
+feat = featureExt.features
+
+biExt = BigramExtractor()
 
 s = "I like ice cream."
-bigrams = getBigrams(s)
+bigrams = biExt.getBigrams(s)
+featuresOn = []
 for b in bigrams:
     if b in feat:
-        print(b)
+        featuresOn.append(b)
     else:
         print(f'{b} not present')
+
+
+
